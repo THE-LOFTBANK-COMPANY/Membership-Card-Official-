f2e96392aaf653d8073e75c7b21c75307e9c2c84
Dhruv Dhayal. <137479629+BlockNotes-4515@users.noreply.github.com>
2024-10-05 15:19:33 +0530

    Added allrounder files in one! 😎😍

---
+++ Loginscript.js
@@ -0,0 +1,11 @@
+/*========================= Background Page Functions with simple Rectangular - Box JS ===========================*/
+const bgAnimation = document.getElementById('bgAnimation');
+
+const numberOfColorBoxes = 400;
+
+for (let i = 0; i < numberOfColorBoxes; i++) {
+    const colorBox = document.createElement('div');
+    colorBox.classList.add('colorBox');
+    bgAnimation.append(colorBox)
+}
+
